64d82f6eae3d1609573104105f84564fd7cfe7cd
Mani Movassagh <mani.mgh@gmail.com>
2023-05-27 23:01:42 +0200

    TS

---
+++ cypress/e2e/sample.ts
@@ -0,0 +1,18 @@
+const n: Map<string, string> = new Map()
+n.set('1', '1')
+n.set('2', '2')
+console.log(n)
+ const p = n.get('1')
+console.log(p)
+n.delete('1')
+const obj={
+     a:1
+}
+const s=new Set([1,2,3])
+
+console.log(s)
+s.add(4)
+console.log(s)
+s.delete(1)
+console.log(s)
+console.log(s.has(1))
