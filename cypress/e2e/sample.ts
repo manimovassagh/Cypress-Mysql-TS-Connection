e0fb053dd831da0a35960ee5b39f86faaf14dccb
Mani Movassagh <mani.mgh@gmail.com>
2023-06-04 11:57:42 +0200

    factory function

--- cypress/e2e/sample.ts
+++ cypress/e2e/sample.ts
@@ -11,4 +11,14 @@ const obj = {
 const ﬂ = new Set([1, 2, 3])
 
 
+const factory = () =>{
+    return {
+        name:(n:string)=>console.log(n),
+        family:(f:string)=>console.log(f),
+        fullName:(n:string,f:string)=>console.log(n,f)
+        
+    }
+}
 
+const logger=factory()
+logger.family("movassagh")
