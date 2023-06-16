81d775f351156a96dbfe54767b842898c29e6914
Mani Movassagh <mani.mgh@gmail.com>
2023-06-16 23:00:45 +0200

    c

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -4,7 +4,7 @@ describe('template spec', () => {
   const wrapper: IWrapLogicSpec = (num, name, arr) => {
     return { name: name, num: num, arr: arr }
     //change test behavior
-    //chang
+    //chang again
   }
   wrapper(14, 'mani', ['mani', 'sahar'])
 
